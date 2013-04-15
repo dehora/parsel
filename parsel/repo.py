2a76fa16aec25d95cac6e26babf9306fd5c685dd
Bill de hOra <bill@dehora.net>
2013-04-15 20:11:19 +0100

    add apache debian to ami prepare

--- parsel/repo.py
+++ parsel/repo.py
@@ -14,7 +14,7 @@ def install_datastax_debian_repos(config):
     exe('wget -O - http://debian.datastax.com/debian/repo_key | sudo apt-key add -')
     exe('sudo rm /etc/apt/sources.list.d/datastax.sources.list', log=False, expectError=True)
     pipe('echo "deb http://debian.datastax.com/community stable main"',
-                'sudo tee -a /etc/apt/sources.list.d/datastax.sources.list')
+        'sudo tee -a /etc/apt/sources.list.d/datastax.sources.list')
     pipe('curl -s http://installer.datastax.com/downloads/ubuntuarchive.repo_key', 'sudo apt-key add -')
     pipe('curl -s http://opscenter.datastax.com/debian/repo_key', 'sudo apt-key add -')
     pipe('curl -s http://debian.datastax.com/debian/repo_key', 'sudo apt-key add -')
@@ -23,6 +23,24 @@ def install_datastax_debian_repos(config):
     # Never install again
     logger.info("install_datastax_debian_repos done  .\n")
     config.set_config("instance", "datastax_debian_attempted", True)
+
+
+def install_apache_debian_repos(config, version="11x"):
+    if config.get_config("instance", "apache_debian_attempted"):
+        return
+    exe('gpg --keyserver pgp.mit.edu --recv-keys 2B5C1B00')
+    exe('gpg --export --armor 2B5C1B00 | sudo apt-key add -')
+    exe('sudo rm /etc/apt/sources.list.d/apache_cassandra.sources.list', log=False, expectError=True)
+    pipe('echo "deb http://www.apache.org/dist/cassandra/debian %s main"' % version,
+        'sudo tee -a /etc/apt/sources.list.d/apache_cassandra.sources.list')
+    pipe('echo "deb-src http://www.apache.org/dist/cassandra/debian %s main"' % version,
+         'sudo tee -a /etc/apt/sources.list.d/apache_cassandra.sources.list')
+    exe('sudo apt-get update')
+    time.sleep(5)
+    # Never install again
+    logger.info("apache_debian_attempted done  .\n")
+    config.set_config("instance", "apache_debian_attempted", True)
+
 
 if __name__ == "__main__":
     install_datastax_debian_repos(Config())
--- parsel/rund/cassandra.py
+++ parsel/rund/cassandra.py
@@ -13,22 +13,35 @@ class CassandraInstaller:
 
     def install(self, config, options):
         if config.get_config("instance", "cassandra_configuration") != "completed":
-            repo.install_datastax_debian_repos(config)
-            self.install_cassandra(config, options)
+            if options.release and options.release.startswith('dsc'):
+                repo.install_datastax_debian_repos(config)
+                self.install_cassandra_datastax(config, options)
+            elif options.release and options.release.startswith('asf'):
+                cass_release = options.release[3:]
+                repo.install_apache_debian_repos(config, cass_release)
+                self.install_cassandra_apache(config, options.release)
+            else:
+                pass  # fail
             config.set_config("instance", "cassandra_configuration", "completed")
         else:
             logger.info('CassandraInstaller logged as completed, skipping.')
 
-    def install_cassandra(self, config, options):
-        if options.release and options.release.startswith('1.0'):
+    def install_cassandra_apache(self, config, cass_release="asf11x"):
+        logger.info('Installing apache cassandra cassandra=%s' % (cass_release,))
+        config.set_config('cassandra', 'partitioner', 'random_partitioner')
+        logger.exe('sudo apt-get install cassandra')
+        config.set_config('instance', 'package', cass_release)
+
+    def install_cassandra_datastax(self, config, options):
+        if options.release and options.release.startswith('dsc1.0'):
             cass_release = options.release
             if cass_release == '1.0.11-1':
                 cass_release = '1.0.11'
-            logger.info('Installing cassandra cassandra=%s dsc=%s' % (cass_release, options.release))
+            logger.info('Installing datastax cassandra cassandra=%s dsc=%s' % (cass_release, options.release))
             logger.exe('sudo apt-get install -y python-cql cassandra={0} dsc={1}'.format(cass_release, options.release))
             config.set_config('instance', 'package', 'dsc')
             config.set_config('cassandra', 'partitioner', 'random_partitioner')
-        elif options.release and options.release.startswith('1.1'):
+        elif options.release and options.release.startswith('dsc1.1'):
             dse_release = cass_release = options.release
             if dse_release == '1.1.6':
                 dse_release = '1.1.6-1'
@@ -37,7 +50,7 @@ class CassandraInstaller:
                 cass_release = "1.1.9"
                 dse_release = "1.1.9-1"
                 logger.info('Cassandra 1.1 requested, installing 1.1.9 as highest available')
-            logger.info('Installing cassandra cassandra=%s dsc=%s' % (cass_release, dse_release))
+            logger.info('Installing datastax cassandra cassandra=%s dsc=%s' % (cass_release, dse_release))
             logger.exe('sudo apt-get install -y python-cql cassandra={0} dsc1.1={1}'.format(cass_release, dse_release))
             config.set_config('instance', 'package', 'dsc1.1')
             config.set_config('cassandra', 'partitioner', 'random_partitioner')
